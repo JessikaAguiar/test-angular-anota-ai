8a360fc4b7fee6aa7da70e642f4fb2abf48170de
Jéssika Aguiar <jessika.developer@gmail.com>
2024-06-15 23:19:45 -0300

    loading cards

---
+++ src/app/shared/components/loadign/loading.component.ts
@@ -0,0 +1,11 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'loading',
+  standalone: true,
+  imports: [],
+  templateUrl: './loading.component.html',
+  styleUrl: './loading.component.scss'
+})
+export class LoadingComponent {
+}
